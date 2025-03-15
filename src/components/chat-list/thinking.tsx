cbf28b6d2b736959d7c5123c949e590d5b8c5a3a
Aquamarine <2418013844@qq.com>
2025-03-15 11:59:33 +0800

    feat:thinking component

--- src/components/chat-list/thinking.tsx
+++ src/components/chat-list/thinking.tsx
@@ -11,12 +11,13 @@ export interface ThinkingProps {
 export function Thinking(props: ThinkingProps) {
     return <div className={"w-fit max-w-full"}>
         <Accordion type={"single"} className={"w-full"} collapsible>
-
+            
             <AccordionItem value={"item-1"} >
                 <AccordionTrigger className={"w-full select-none hover:no-underline "}>
                     <span className={"select-none text-foreground/60 font-semibold hover:text-foreground/80 transition hover:cursor-pointer"}>
-                        正在思考,用时{(Date.now() - props.message.thinking!.startTime) / 1000}秒
+                        正在思考,用时{(Date.now() -   props.message.thinking!.startTime) / 1000}秒
                     </span>
+                    
                 </AccordionTrigger>
                 <AccordionContent className={"border-x-2 border-foreground/20 px-3 py-3 break-words "}>
                     <motion.div
